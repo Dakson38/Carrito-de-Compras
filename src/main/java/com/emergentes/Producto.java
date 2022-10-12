950fbdab6de48a81122baa6d7500cc199d16506e
Jose Calancha <daksonvibes38@gmail.com>
2022-10-11 22:16:43 -0400

    Version 1.0

---
+++ src/main/java/com/emergentes/Producto.java
@@ -0,0 +1,49 @@
+package com.emergentes;
+
+/**
+ *
+ * @author CJ
+ */
+public class Producto {
+    private int id;
+    private String producto;
+    private int cantidad;
+    private double precio;
+
+    public Producto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
